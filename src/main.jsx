9a29cc26fcdfc4074fe6f3d2d3e94c738eed1d40
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2023-08-21 21:07:22 +0200

    Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "./index.css"
 
-ReactDOM.render(
+const root = document.getElementById('root');
+const rootContainer = ReactDOM.createRoot(root);
+
+rootContainer.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
