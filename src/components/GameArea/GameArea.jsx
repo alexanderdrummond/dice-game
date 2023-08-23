d79564949f8cbfb8d9b128c064a5b1a29bc7b796
Alexander Drummond <113899680+alexanderdrummond@users.noreply.github.com>
2023-08-23 12:38:10 +0200

    audio fixes

--- src/components/GameArea/GameArea.jsx
+++ src/components/GameArea/GameArea.jsx
@@ -5,6 +5,7 @@ import Confetti from 'react-confetti';
 import styles from './GameArea.module.scss';
 import winnerAudio from '../../winner.mp3';
 import rollAudio from '../../dice.wav';
+import computerAudio from '../../trombone.wav'
 
 function GameArea() {
   const [diceValue, setDiceValue] = useState(0);
@@ -17,6 +18,8 @@ function GameArea() {
   const [player2Score, setPlayer2Score] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
   const [winner, setWinner] = useState(null);
+
+  // controls for confetti size
 
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
@@ -38,16 +41,20 @@ function GameArea() {
     };
   }, []);
 
+  // roll handler start
+
   const handleRoll = (setter, isComputer) => {
     const roll = Math.floor(Math.random() * 6) + 1;
     setter(roll);
     setDiceValue(roll);
-  
-    if (!isComputer) {
-      const audio = new Audio(rollAudio);
-      audio.play();
-    }
-  
+
+    // dice audio
+
+    const audio = new Audio(rollAudio);
+    audio.play();
+
+    // PvC gamemode controls
+
     if (gameMode === 'PvC') {
       if (!isComputer) {
         const updatedPlayerScore = playerScore + roll;
@@ -60,12 +67,17 @@ function GameArea() {
         setComputerScore(updatedComputerScore);
         if (updatedComputerScore >= 50) {
           setWinner('Computer');
+          const computerWinAudio = new Audio(computerAudio);
+          computerWinAudio.play();
         }
       }
-  
+
       if (!isComputer) {
         handleRoll(setComputerRoll, true);
       }
+
+      // Player1 controls
+
     } else {
       if (playerTurn === 1) {
         const updatedPlayerScore = playerScore + roll;
@@ -73,6 +85,9 @@ function GameArea() {
         if (updatedPlayerScore >= 50) {
           setWinner('Player 1');
         }
+
+        // Player 2 controls
+
       } else {
         const updatedPlayer2Score = player2Score + roll;
         setPlayer2Score(updatedPlayer2Score);
@@ -80,10 +95,14 @@ function GameArea() {
           setWinner('Player 2');
         }
       }
+
+      // Turn determinator
+
       setPlayerTurn(prevTurn => 3 - prevTurn);
     }
   };
 
+  // controls to reset game - resets states and sets the player turn.
 
   const resetGame = () => {
     setPlayerScore(0);
@@ -96,19 +115,19 @@ function GameArea() {
     setWinner(null);
     setPlayerTurn(1);
   };
-  
+
   return (
     <div className={styles.appcon}>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <h1>Dice Game</h1>
         <div className={styles.controlwrap}>
-        <button className={styles.controls} onClick={() => {
-          setGameMode(prevMode => (prevMode === 'PvC' ? 'PvP' : 'PvC'));
-          resetGame(); 
-        }}>
-          {gameMode === 'PvC' ? "Player vs Player" : "Player vs Computer"}
-        </button>
-        <button className={styles.controls} onClick={resetGame}>Reset game</button></div>
+          <button className={styles.controls} onClick={() => {
+            setGameMode(prevMode => (prevMode === 'PvC' ? 'PvP' : 'PvC'));
+            resetGame();
+          }}>
+            {gameMode === 'PvC' ? "Player vs Player" : "Player vs Computer"}
+          </button>
+          <button className={styles.controls} onClick={resetGame}>Reset game</button></div>
       </div>
       <div className={styles.gameArea}>
         {gameMode === 'PvC' ? (
@@ -128,16 +147,18 @@ function GameArea() {
           </React.Fragment>
         )}
         {winner && (
-          <React.Fragment>
-            <Confetti width={windowSize.width} height={windowSize.height} />
-            <div className={styles.overlayz}>
-              <h2 className={styles.blinking}>{winner} wins! 🎉</h2>
+        <React.Fragment>
+          <Confetti width={windowSize.width} height={windowSize.height} />
+          <div className={styles.overlayz}>
+            <h2 className={styles.blinking}>{winner} wins! 🎉</h2>
+            {winner !== 'Computer' && (
               <audio src={winnerAudio} autoPlay={winner !== null} preload="auto" style={{ display: 'none' }} />
-              <div>
-                <button className={styles.controls} onClick={resetGame}>Start New Game</button>
-              </div>
+            )}
+            <div>
+              <button className={styles.controls} onClick={resetGame}>Start New Game</button>
             </div>
-          </React.Fragment>
+          </div>
+        </React.Fragment>
         )}
       </div>
     </div>
